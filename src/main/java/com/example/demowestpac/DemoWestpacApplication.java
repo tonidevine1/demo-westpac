fd233a09fedaa6ada561b50a246d8443ebf55afa
papicella <pas.apicella@gmail.com>
2023-01-12 15:14:40 +1100

    first commit

---
+++ src/main/java/com/example/demowestpac/DemoWestpacApplication.java
@@ -0,0 +1,13 @@
+package com.example.demowestpac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWestpacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWestpacApplication.class, args);
+    }
+
+}
